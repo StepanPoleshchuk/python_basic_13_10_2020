e63a46ec32c537c9a1cca5ca139e9dbb3510d355
StepanPoleshchuk <stepanpoleshuk@gmail.com>
2020-10-25 12:10:12 +0300

    lesson 4

--- task1.py
+++ task1.py
@@ -13,7 +13,7 @@ while True:
         data_time = float(input('введите выработку в часах\n'))
         data_cashtime = float(input('введите ставку в час\n'))
         data_special = float(input('введите премию\n'))
-        print (f'Заработная плата сотрудника {count(data_time, data_cashtime, data_special)}')
+        print(f'Заработная плата сотрудника {count(data_time, data_cashtime, data_special)}')
         break
     except ValueError:
         print('Вы ввели не числа, пробуем все заново')
